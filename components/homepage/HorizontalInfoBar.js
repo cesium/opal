9fc2dea7323fa21ce20a18ea81f2f1485ff73d97
Luís Alves <24353152+alves-luis@users.noreply.github.com>
2019-09-20 18:19:49 +0100

    Using styled components and JSON file
    Now using styled components, and changed import of static data from JS to JSON file.

--- components/homepage/HorizontalInfoBar.js
+++ components/homepage/HorizontalInfoBar.js
@@ -1,49 +1,48 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { styled } from '@material-ui/core/styles';
 import { Box, Divider, Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-const useStyles = makeStyles(theme => ({
-    wrapper: {
-        width: '100%',
-        paddingLeft: theme.spacing(3),
-        paddingRight: theme.spacing(3),
-        paddingTop: theme.spacing(2),
-        paddingBottom: theme.spacing(2),
-        justifyContent: 'flex-start',
-        display: 'flex',
-    },
-    titleBox: {
-        width: '25%',
-        paddingRight: theme.spacing(3),
-    },
-    title: {
-        textAlign: 'center',
-        textTransform: 'uppercase',
-    },
-    bodyBox: {
-        width: '75%',
-    },
+const Wrapper = styled(Box)({
+    width: '100%',
+    paddingLeft: '2em',
+    paddingRight: '2em',
+    paddingTop: '1em',
+    paddingBottom: '1em',
+    justifyContent: 'flex-start',
+    display: 'flex',
+});
 
-}));
+const TitleBox = styled(Box)({
+    width: '25%',
+    paddingRight: '1em',
+});
 
-export default function HorizontalInfoBar(props) {
-    const classes = useStyles();
+const Title = styled(Typography)({
+    textAlign: 'center',
+    textTransform: 'uppercase',
+});
+
+const BodyBox = styled(Box)({
+    width: '75%',
+})
+
+function HorizontalInfoBar(props) {
 
     return (
-        <Box className={classes.wrapper}>
-            <Box className={classes.titleBox}>
-                <Typography variant="h5" className={classes.title}>
+        <Wrapper>
+            <TitleBox>
+                <Title variant="h5">
                     {props.title}
-                </Typography>
-            </Box>
-            <Box className={classes.bodyBox}>
+                </Title>
+            </TitleBox>
+            <BodyBox>
                 {props.body.map((line, i) =>
                     <Typography key={i} variant="body1" align="left">
                         {line}
                     </Typography>
                 )}
-            </Box>
-        </Box>
+            </BodyBox>
+        </Wrapper>
     );
 }
 
@@ -51,3 +50,5 @@ HorizontalInfoBar.propTypes = {
     title: PropTypes.string.isRequired,
     body: PropTypes.array.isRequired,
 };
+
+export default HorizontalInfoBar;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Layout from '../components/Layout';
 import HorizontalInfoBar from '../components/homepage/HorizontalInfoBar';
-import home from '../static/homepage.js'
+import home from '../static/homepage.json'
 
 const Index = () => (
   <Layout>
