42cff630517d3c351934529cc88c33088493e37e
Luís Alves <luismig.alves@gmail.com>
2019-09-25 21:15:26 +0100

    Changed to Grid components, I believe it makes more sense (and returned to array of strings)

--- components/homepage/HorizontalInfoBar.js
+++ components/homepage/HorizontalInfoBar.js
@@ -1,19 +1,18 @@
 import { styled } from '@material-ui/core/styles';
-import { Box, Divider, Typography } from '@material-ui/core';
+import { Divider, Typography, Grid } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-const Wrapper = styled(Box)({
-    width: '100%',
+const Wrapper = styled(Grid)({
     paddingLeft: '2em',
     paddingRight: '2em',
     paddingTop: '1em',
     paddingBottom: '1em',
-    justifyContent: 'flex-start',
-    display: 'flex',
+    direction: 'row',
+    justify: 'space-between',
+    alignItems: 'flex-start',
 });
 
-const TitleBox = styled(Box)({
-    width: '25%',
+const TitleBox = styled(Grid)({
     paddingRight: '1em',
 });
 
@@ -22,8 +21,8 @@ const Title = styled(Typography)({
     textTransform: 'uppercase',
 });
 
-const BodyBox = styled(Box)({
-    width: '75%',
+const BodyBox = styled(Grid)({
+    paddingLeft: '1em',
 });
 
 const Body = styled(Typography)({
@@ -33,13 +32,13 @@ const Body = styled(Typography)({
 function HorizontalInfoBar(props) {
 
     return (
-        <Wrapper>
-            <TitleBox>
+        <Wrapper container>
+            <TitleBox xs="3">
                 <Title variant="h5">
                     {props.title}
                 </Title>
             </TitleBox>
-            <BodyBox>
+            <BodyBox xs="9">
                 {props.body.map((line, i) =>
                     <Body key={i} variant="body1">
                         {line}
