7fe37b2df2497d7d71564fd2a2066f080786d327
Luís Alves <luismig.alves@gmail.com>
2019-09-27 14:19:33 +0100

    Added the homepage component to display featured speakers, first iteration

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import FeaturedImages from '../components/homepage/FeaturedImages';
+import home from '../static/homepage.json'
 import Layout from '../components/Layout';
 
 const Index = () => (
   <Layout>
+  <FeaturedImages title={home.featuredSpeakers.title} featured={home.featuredSpeakers.featured} />
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1">
