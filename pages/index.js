82136ad6437e02f70f968c66b6512536d5079fe8
Luís Alves <24353152+alves-luis@users.noreply.github.com>
2019-12-14 11:18:00 +0000

    Added How to get there component (#24)

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Banner from '../components/homepage/Banner';
 import home from '../data/homepage.json';
 import Layout from '../components/Layout';
 import HorizontalInfoBar from '../components/homepage/HorizontalInfoBar';
+import HowToGetHere from '../components/homepage/HowToGetHere';
 import Pricing from '../components/homepage/Pricing';
 
 const Index = () => (
@@ -21,6 +22,12 @@ const Index = () => (
       title={home.about.title}
       body={home.about.body}
       url={home.about.teaserURL}
+    />
+    <HowToGetHere
+      location={home.where.location}
+      subtitle={home.where.subtitle}
+      means={home.where.means}
+      background={home.where.background}
     />
     <Pricing tiers={home.ticketInfo} />
   </Layout>
